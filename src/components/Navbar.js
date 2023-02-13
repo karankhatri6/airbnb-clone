fdc74589398fb38923f60485188729934a319475
Karan Khatri <karankhatri535@gmail.com>
2023-02-13 20:07:40 +0530

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from "../images/airbnb 1.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src = {Logo} alt = "AirBnb Logo" />
+    </nav>
+  );
+};
+
+export default Navbar;
